444b96744befdb5db54d920f490f3c84a2bb7938
Mohamed Sayed <mohamedsayedzkh@gmail.com>
2024-07-14 00:28:40 +0300

    changed main() entry point
    added a demonstration for usage

--- UI.c
+++
@@ -1,3 +0,0 @@
-void prompt_user(){
-    
-}
--- main.c
+++ main.c
@@ -6,11 +6,20 @@
 
 int main(int argc, char const *argv[])
 {
-    struct node *first_node = create_ll(10);
-    append_node(first_node, 11);
-    append_node(first_node, 15125);
-    print_ll(first_node);
-    destroy_ll(first_node);
+    /* Entry point of the program is here. */
+    // Creates a linked list with 1 node that stores the number 1
+    struct node *MyLinkedList = create_ll(1);
+    // Recursively adds numbers from 2 to 100 to the linked list
+    for (size_t i = 2; i <= 100; i++){
+        append_node(MyLinkedList, i);
+    }
+    // Prints the linked list
+    print_ll(MyLinkedList);
+    // Destroys the linked list and frees all the memory that was allocated to it, Thus making MyLinkedList a dangling pointer.
+    // MyLinkedList shouldn't be used unless it is reassigned to a new memory location, undefined behaviour will ensue if it is used.
+    destroy_ll(MyLinkedList);
+    // Pauses the program so that the terminal doesn't close immediately after startup
+    getchar();
 
     return 0;
 }
